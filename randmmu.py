9ba24fe2d5e2a1768fc8a54a50b0605a79a262ca
hoangson <lehoangsonsg@gmail.com>
2023-08-31 16:31:42 +0930

    fix: fix randmmu bug adding item to active list before removing

--- randmmu.py
+++ randmmu.py
@@ -9,18 +9,21 @@ class RandMMU(MMU):
 
     def _post_evict_frame_hook(self, page_numer):
         self.active_frames.remove(page_numer)
+        self.log(f"Post evict: {self.active_frames}")
 
     def _post_add_frame_hook(self, page_number):
         self.active_frames.append(page_number)
+        self.log(f"Post add: {self.active_frames}")
 
     def _get_evicted_frame(self):
         if self.frame_count == 0:
             raise ValueError("No item to evict")
-        frame_id = random.randint(0, self.frame_count - 1)
+        frame_id = random.randint(0, self.frame_count - 2)
         return self.active_frames[frame_id]
 
+
 if __name__ == "__main__":
-    mmu = RandMMU(10)
+    mmu = RandMMU(3)
     mmu.read_memory(1)
     mmu.read_memory(2)
     mmu.read_memory(3)
