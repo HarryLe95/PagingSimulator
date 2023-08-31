444351c6c7637976d3ab436ae82ece6132198f01
hoangson <lehoangsonsg@gmail.com>
2023-08-31 16:32:07 +0930

    fix: fix lrummu bug due to post hooks

--- lrummu.py
+++ lrummu.py
@@ -9,33 +9,31 @@ class LruMMU(MMU):
 
     def _post_evict_frame_hook(self, page_numer):
         self.active_frames.remove(page_numer)
+        self.log(f"Post evict: {self.active_frames}")
 
     def _post_add_frame_hook(self, page_number):
         if page_number in self.active_frames:
             self.active_frames.remove(page_number)
         self.active_frames.append(page_number)
+        self.log(f"Post add: {self.active_frames}")
 
     def _get_evicted_frame(self):
         if len(self.active_frames) != 0:
             return self.active_frames[0]
         raise ValueError("Memory is empty")
 
+
 if __name__ == "__main__":
-    mmu = LruMMU(10)
+    mmu = LruMMU(3)
+    mmu.read_memory(0)
     mmu.read_memory(1)
     mmu.read_memory(2)
+    mmu.read_memory(0)
+    mmu.read_memory(1)
     mmu.read_memory(3)
-    mmu.read_memory(4)
-    mmu.read_memory(5)
-    mmu.read_memory(6)
-    mmu.read_memory(7)
-    mmu.read_memory(8)
-    mmu.read_memory(9)
-    mmu.write_memory(1)
-    mmu.write_memory(2)
-    mmu.read_memory(4)
-    mmu.read_memory(11)
-    mmu.read_memory(12)
-    mmu.read_memory(13)
-    mmu.read_memory(14)
+    mmu.read_memory(0)
+    mmu.read_memory(3)
+    mmu.read_memory(1)
+    mmu.read_memory(2)
+    mmu.read_memory(1)
     print("End")
